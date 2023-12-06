8a9052acb551c48cc6b999690dd83536e0e856b5
ravi-gangwar <ravigangwar7465@gmail.com>
2023-12-07 00:33:53 +0530

    added  data file

--- src/components/ForeGround.jsx
+++ src/components/ForeGround.jsx
@@ -1,39 +1,8 @@
-import React from "react"; 
 import Card from "./Card"
+import data from "../data/data.js"
 
 function ForeGround() {
-    const data = [
-        {
-            desc : "Lorem ipsum dolor sit, amet consectetur adipisicing.",
-            fileSize : ".9mb",
-            close: false,
-            tag : {
-                isOpen : true,
-                tagTitle : "Downlaod Now",
-                tagColor : "bg-green-600",
-            }
-        },
-        {
-            desc : "Lorem ipsum dolor sit, amet consectetur adipisicing.",
-            fileSize : ".9mb",
-            close: false,
-            tag : {
-                isOpen : true,
-                tagTitle : "Downlaod Now",
-                tagColor : "bg-green-600",
-            }
-        },
-        {
-            desc : "Lorem ipsum dolor sit, amet consectetur adipisicing.",
-            fileSize : ".9mb",
-            close: false,
-            tag : {
-                isOpen : true,
-                tagTitle : "Upload",
-                tagColor : "bg-sky-500",
-            }
-        },
-    ]
+    
   return (
     <>
         <div className='flex gap-5 flex-wrap w-full h-full fixed top-0 left-0 z-[3] p-5'>
---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        desc : "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        fileSize : ".9mb",
+        close: false,
+        tag : {
+            isOpen : true,
+            tagTitle : "Downlaod Now",
+            tagColor : "bg-zinc-600",
+        }
+    },
+    {
+        desc : "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        fileSize : ".9mb",
+        close: false,
+        tag : {
+            isOpen : true,
+            tagTitle : "Downlaod Now",
+            tagColor : "bg-green-600",
+        }
+    },
+    {
+        desc : "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        fileSize : ".9mb",
+        close: false,
+        tag : {
+            isOpen : true,
+            tagTitle : "Upload",
+            tagColor : "bg-sky-500",
+        }
+    },
+]
+
+
+export default data;
